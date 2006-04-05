272fc4e7f212da49c7a5366e4cf3550012a731dc
nobody <nobody@localhost>
2006-04-05 19:38:34 +0000

    This commit was manufactured by cvs2svn to create branch 'multi_array_python'.
    [SVN r33562]

--- test/resize.cpp
+++ test/resize.cpp
@@ -35,13 +35,6 @@ int test_main(int,char*[]) {
     20,21,22,23
   };
 
-
-  marray A(boost::extents[2][3][4]);
-
-  A.assign(A_data,A_data+(2*3*4));
-
-  A.resize(boost::extents[4][3][2]);
-  
   int A_resize[] = {
     0,1,
     4,5,
@@ -60,14 +53,67 @@ int test_main(int,char*[]) {
     0,0
   };
 
-  BOOST_CHECK(std::equal(A_resize,A_resize+(4*3*2),A.data()));
+  // resize through the extent_gen interface
+  {
+    marray A(boost::extents[2][3][4]);
+    A.assign(A_data,A_data+(2*3*4));
+    A.resize(boost::extents[4][3][2]);
+    BOOST_CHECK(std::equal(A_resize,A_resize+(4*3*2),A.data()));
+  }  
 
+  // resize through the Collection
+  {
+    marray A(boost::extents[2][3][4]);
+    A.assign(A_data,A_data+(2*3*4));
+    boost::array<int,3> new_extents = {{4,3,2}};
+    A.resize(new_extents);
+    BOOST_CHECK(std::equal(A_resize,A_resize+(4*3*2),A.data()));
+  }  
 
+  // default construct all the new elements (in this case, all elements)
   {
     marray defaultA;
     defaultA.resize(boost::extents[2][3][4]);
     BOOST_CHECK(std::accumulate(defaultA.data(),
                                defaultA.data()+(2*3*4),0) == 0);
   }
+
+
+
+  // verify the preservation of storage order
+  {
+    int tiling_graph_storage_order[] = {2, 0, 1}; 
+    bool tiling_graph_index_order[] = {true, true, true};
+  
+    marray A(boost::extents[3][4][2],
+             boost::general_storage_order<3>(tiling_graph_storage_order,
+                                             tiling_graph_index_order)); 
+
+
+    int value = 0;
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 4; j++) {
+        for (int k = 0; k < 2; k++) {
+          *(A.data() + value) = value;
+          ++value;
+        }
+      }
+    }
+
+    // "Resize" to the same size
+    A.resize(boost::extents[3][4][2]);
+
+    int check = 0;
+    for (int i = 0; i < 3; i++) { 
+      for (int j = 0; j < 4; j++) {
+        for (int k = 0; k < 2; k++) {
+          BOOST_CHECK(*(A.data() + check) == check);
+          ++check;
+        }
+      }
+    }
+  }
+
+
   return boost::exit_success;
 }
