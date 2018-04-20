1271eaf422784493dc263188db147f9f96d8234e
CromwellEnage <32967088+CromwellEnage@users.noreply.github.com>
2018-04-20 06:20:37 -0400

    Use <boost/core/lightweight_test.hpp>
    Switched from Boost.Test to Boost.Core lightweight_test component.

--- test/generative_tests.hpp
+++ test/generative_tests.hpp
@@ -3,8 +3,8 @@
 
 // Copyright 2002 The Trustees of Indiana University.
 
-// Use, modification and distribution is subject to the Boost Software 
-// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
 //  Boost.MultiArray Library
@@ -19,11 +19,12 @@
 //
 //  In order to create a set of tests, you must define the following two
 //  function signatures:
-//   template <typename Array>
-//   void access(Array& A, const mutable_array_tag&);
 //
-//   template <typename Array>
-//   void access(Array& A, const const_array_tag&);
+//      template <typename Array>
+//      unsigned int access(Array& A, mutable_array_tag const&);
+//
+//      template <typename Array>
+//      unsigned int access(Array& A, const_array_tag const&);
 //
 //  The framework will always pass 2x3x4 arrays into these functions.
 //  The const_array_tag version of access must NOT attempt to modify 
@@ -37,249 +38,299 @@
 //  #define MULTIARRAY_TEST_ASSIGN before including this file.
 //  assign() will call this function.
 //
-//  If you wish to know how many tests were run, you must increment
-//  the global variable 'tests_run' somewhere in your test code.
+//  Each access() function should return the number of tests that it ran.
 //
-//  Since generative-tests uses the Boost.Test framework, you must
-//  define at least the following:
+//  Since generative-tests uses the Boost.LightweightTest framework,
+//  you must define at least the following:
 //
-//  int test_main(int,char*[]) { return run_generative_tests(); }
+//  int main(int, char*[]) { return run_generative_tests(); }
 //
-#include "boost/multi_array.hpp"
 
-#include "boost/test/minimal.hpp"
-
-#include <boost/config.hpp> /* BOOST_NO_SFINAE */
+#include <boost/multi_array.hpp>
+#include <boost/core/lightweight_test.hpp>
+#include <boost/config.hpp> // BOOST_NO_SFINAE
 #include <algorithm>
 #include <iostream>
 #include <vector>
 
-namespace {
-  unsigned int tests_run = 0;
-} // empty namespace 
+struct mutable_array_tag
+{
+};
 
-struct mutable_array_tag { };
-struct const_array_tag { };
+struct const_array_tag
+{
+};
 
 template <typename Array>
-void assign_if_not_const(Array&, const const_array_tag&) {
-  // do nothing
+void assign_if_not_const(Array&, const_array_tag const&)
+{
+    // do nothing
 }
 
 template <typename Array>
-void assign_if_not_const(Array& A, const mutable_array_tag&);
+void assign_if_not_const(Array& A, mutable_array_tag const&);
 
-#ifndef MULTIARRAY_TEST_ASSIGN
+#if !defined(MULTIARRAY_TEST_ASSIGN)
 template <typename Array>
-void assign_if_not_const(Array& A, const mutable_array_tag&) {
+void assign_if_not_const(Array& A, mutable_array_tag const&)
+{
+    typedef typename Array::index index;
 
-  typedef typename Array::index index;
+    index const idx0 = A.index_bases()[0];
+    index const idx1 = A.index_bases()[1];
+    index const idx2 = A.index_bases()[2];
 
-  const index idx0 = A.index_bases()[0];
-  const index idx1 = A.index_bases()[1];
-  const index idx2 = A.index_bases()[2];
+    int num = 0;
 
-
-  int num = 0;
-  for (index i = idx0; i != idx0 + 2; ++i)
-    for (index j = idx1; j != idx1 + 3; ++j)
-      for (index k = idx2; k != idx2 + 4; ++k) 
-        A[i][j][k] = num++;
+    for (index i = idx0; i != idx0 + 2; ++i)
+    {
+        for (index j = idx1; j != idx1 + 3; ++j)
+        {
+            for (index k = idx2; k != idx2 + 4; ++k)
+            {
+                A[i][j][k] = num++;
+            }
+        }
+    }
 }
 #endif // MULTIARRAY_TEST_ASSIGN
 
 template <typename Array>
-void assign(Array& A) {
-  assign_if_not_const(A,mutable_array_tag());
+void assign(Array& A)
+{
+    assign_if_not_const(A, mutable_array_tag());
 }
 
 template <typename Array>
-void access(Array& A, const mutable_array_tag&);
+unsigned int access(Array& A, mutable_array_tag const&);
 
 template <typename Array>
-void access(Array& A, const const_array_tag&);
+unsigned int access(Array& A, const_array_tag const&);
 
-template <typename StorageOrder3,typename StorageOrder4,typename Modifier>
-int run_configuration(const StorageOrder3& so3,
-                      const StorageOrder4& so4,
-                      const Modifier& modifier) {
-  // multi_array
-  {
-    typedef boost::multi_array<int,3> array;
-    typename array::extent_gen extents;
+template <
+    typename StorageOrder3
+  , typename StorageOrder4
+  , typename Modifier
+>
+unsigned int
+    run_configuration(
+        StorageOrder3 const& so3
+      , StorageOrder4 const& so4
+      , Modifier const& modifier
+    )
+{
+    unsigned int tests_run = 0;
+
+    // multi_array
     {
-      array A(extents[2][3][4],so3);
-      modifier.modify(A);
-      access(A,mutable_array_tag());
+        typedef boost::multi_array<int,3> array;
+        typename array::extent_gen extents;
+        {
+            array A(extents[2][3][4],so3);
+            modifier.modify(A);
+            tests_run += access(A, mutable_array_tag());
+        }
     }
-  }
-  // multi_array_ref
-  {
-    typedef boost::multi_array_ref<int,3> array_ref;
-    typename array_ref::extent_gen extents;
+
+    // multi_array_ref
     {
-      int local[24];
-      array_ref A(local,extents[2][3][4],so3);
-      modifier.modify(A);
-      access(A,mutable_array_tag());
+        typedef boost::multi_array_ref<int,3> array_ref;
+        typename array_ref::extent_gen extents;
+        {
+            int local[24];
+            array_ref A(local, extents[2][3][4], so3);
+            modifier.modify(A);
+            tests_run += access(A, mutable_array_tag());
+        }
     }
-  }
-  // const_multi_array_ref
-  {
-    typedef boost::multi_array_ref<int,3> array_ref;
-    typedef boost::const_multi_array_ref<int,3> const_array_ref;
-    typename array_ref::extent_gen extents;
+
+    // const_multi_array_ref
     {
-      int local[24];
-      array_ref A(local,extents[2][3][4],so3);
-      modifier.modify(A);
-      assign(A);
+        typedef boost::multi_array_ref<int,3> array_ref;
+        typedef boost::const_multi_array_ref<int,3> const_array_ref;
+        typename array_ref::extent_gen extents;
+        {
+            int local[24];
+            array_ref A(local, extents[2][3][4], so3);
+            modifier.modify(A);
+            assign(A);
 
-      const_array_ref B = A;
-      access(B,const_array_tag());
+            const_array_ref B = A;
+            tests_run += access(B, const_array_tag());
+        }
     }
-  }
-  // sub_array
-  {
-    typedef boost::multi_array<int,4> array;
-    typename array::extent_gen extents;
+
+    // sub_array
     {
-      array A(extents[2][2][3][4],so4);
-      modifier.modify(A);
-      typename array::template subarray<3>::type B = A[1];
-      access(B,mutable_array_tag());
+        typedef boost::multi_array<int,4> array;
+        typename array::extent_gen extents;
+        {
+            array A(extents[2][2][3][4],so4);
+            modifier.modify(A);
+            typename array::template subarray<3>::type B = A[1];
+            tests_run += access(B, mutable_array_tag());
+        }
     }
-  }
-  // const_sub_array
-  {
-    typedef boost::multi_array<int,4> array;
-    typename array::extent_gen extents;
+
+    // const_sub_array
     {
-      array A(extents[2][2][3][4],so4);
-      modifier.modify(A);
-      typename array::template subarray<3>::type B = A[1];
-      assign(B);
+        typedef boost::multi_array<int,4> array;
+        typename array::extent_gen extents;
+        {
+            array A(extents[2][2][3][4],so4);
+            modifier.modify(A);
+            typename array::template subarray<3>::type B = A[1];
+            assign(B);
 
-      typename array::template const_subarray<3>::type C = B;
-      access(C,const_array_tag());
+            typename array::template const_subarray<3>::type C = B;
+            tests_run += access(C, const_array_tag());
+        }
     }
-  }
-  // array_view
-  {
-    typedef boost::multi_array<int,3> array;
-    typedef typename array::index_range range;
-    typename array::index_gen indices;
-    typename array::extent_gen extents;
+
+    // array_view
     {
-      typedef typename array::index index;
+        typedef boost::multi_array<int,3> array;
+        typedef typename array::index_range range;
+        typename array::index_gen indices;
+        typename array::extent_gen extents;
+        {
+            typedef typename array::index index;
 
-      array A(extents[4][5][6],so3);
-      modifier.modify(A);
-      const index idx0 = A.index_bases()[0];
-      const index idx1 = A.index_bases()[1];
-      const index idx2 = A.index_bases()[2];
+            array A(extents[4][5][6],so3);
+            modifier.modify(A);
+            index const idx0 = A.index_bases()[0];
+            index const idx1 = A.index_bases()[1];
+            index const idx2 = A.index_bases()[2];
 
-      typename array::template array_view<3>::type B =A[
-        indices[range(idx0+1,idx0+3)]
-               [range(idx1+1,idx1+4)]
-               [range(idx2+1,idx2+5)]
-      ];
-      access(B,mutable_array_tag());
+            typename array::template array_view<3>::type B = A[
+                indices
+                [range(idx0 + 1, idx0 + 3)]
+                [range(idx1 + 1, idx1 + 4)]
+                [range(idx2 + 1, idx2 + 5)]
+            ];
+            tests_run += access(B, mutable_array_tag());
+        }
     }
-  }
-  // const_array_view
-  {
-    typedef boost::multi_array<int,3> array;
-    typedef typename array::index_range range;
-    typename array::index_gen indices;
-    typename array::extent_gen extents;
+
+    // const_array_view
     {
-      typedef typename array::index index;
+        typedef boost::multi_array<int,3> array;
+        typedef typename array::index_range range;
+        typename array::index_gen indices;
+        typename array::extent_gen extents;
+        {
+            typedef typename array::index index;
 
-      array A(extents[4][5][6],so3);
-      modifier.modify(A);
-      const index idx0 = A.index_bases()[0];
-      const index idx1 = A.index_bases()[1];
-      const index idx2 = A.index_bases()[2];
+            array A(extents[4][5][6],so3);
+            modifier.modify(A);
+            index const idx0 = A.index_bases()[0];
+            index const idx1 = A.index_bases()[1];
+            index const idx2 = A.index_bases()[2];
 
-      typename array::template array_view<3>::type B =A[
-        indices[range(idx0+1,idx0+3)]
-               [range(idx1+1,idx1+4)]
-               [range(idx2+1,idx2+5)]
-      ];
-      assign(B);
+            typename array::template array_view<3>::type B = A[
+                indices
+                [range(idx0 + 1, idx0 + 3)]
+                [range(idx1 + 1, idx1 + 4)]
+                [range(idx2 + 1, idx2 + 5)]
+            ];
+            assign(B);
 
-      typename array::template const_array_view<3>::type C = B;
-      access(C,const_array_tag());
+            typename array::template const_array_view<3>::type C = B;
+            tests_run += access(C, const_array_tag());
+        }
     }
-  }
-  return boost::exit_success;
+
+    return tests_run;
 }
 
 template <typename ArrayModifier>
-int run_storage_tests(const ArrayModifier& modifier) {
-  run_configuration(boost::c_storage_order(),
-                    boost::c_storage_order(),modifier);
-  run_configuration(boost::fortran_storage_order(),
-                    boost::fortran_storage_order(),modifier);
-  
-  std::size_t ordering[] = {2,0,1,3};
-  bool ascending[] = {false,true,true,true};
-  run_configuration(boost::general_storage_order<3>(ordering,ascending),
-                    boost::general_storage_order<4>(ordering,ascending),
-                    modifier); 
-
-  return boost::exit_success;
+unsigned int run_storage_tests(ArrayModifier const& modifier)
+{
+    unsigned int tests_run = run_configuration(
+        boost::c_storage_order()
+      , boost::c_storage_order()
+      , modifier
+    );
+    tests_run += run_configuration(
+        boost::fortran_storage_order()
+      , boost::fortran_storage_order()
+      , modifier
+    );
+    std::size_t ordering[] = {2,0,1,3};
+    bool ascending[] = {false, true, true, true};
+    return tests_run + run_configuration(
+        boost::general_storage_order<3>(ordering, ascending)
+      , boost::general_storage_order<4>(ordering, ascending)
+      , modifier
+    );
 }
 
-struct null_modifier {
-  template <typename Array>
-  void modify(Array&) const { }
+struct null_modifier
+{
+    template <typename Array>
+    void modify(Array&) const
+    {
+    }
 };
 
-struct set_index_base_modifier {
-  template <typename Array>
-  void modify(Array& A) const {
-#ifdef BOOST_NO_SFINAE
-    typedef boost::multi_array_types::index index;
-    A.reindex(index(1));
+struct set_index_base_modifier
+{
+    template <typename Array>
+    void modify(Array& A) const
+    {
+#if defined(BOOST_NO_SFINAE)
+        typedef boost::multi_array_types::index index;
+        A.reindex(index(1));
 #else
-    A.reindex(1);
+        A.reindex(1);
 #endif 
-  }
+    }
 };
 
-struct reindex_modifier {
-  template <typename Array>
-  void modify(Array& A) const {
-    boost::array<int,4> bases = {{1,2,3,4}};
-    A.reindex(bases);
- }
+struct reindex_modifier
+{
+    template <typename Array>
+    void modify(Array& A) const
+    {
+        boost::array<int,4> bases = {{1,2,3,4}};
+        A.reindex(bases);
+    }
 };
 
-struct reshape_modifier {
-  template <typename Array>
-  void modify(Array& A) const {
-    typedef typename Array::size_type size_type;
-    std::vector<size_type> old_shape(A.num_dimensions());
-    std::vector<size_type> new_shape(A.num_dimensions());
+struct reshape_modifier
+{
+    template <typename Array>
+    void modify(Array& A) const
+    {
+        typedef typename Array::size_type size_type;
+        std::vector<size_type> old_shape(A.num_dimensions());
+        std::vector<size_type> new_shape(A.num_dimensions());
 
-    std::copy(A.shape(),A.shape()+A.num_dimensions(),old_shape.begin());
-    std::copy(old_shape.rbegin(),old_shape.rend(),new_shape.begin());
+        std::copy(
+            A.shape()
+          , A.shape() + A.num_dimensions()
+          , old_shape.begin()
+        );
+        std::copy(
+            old_shape.rbegin()
+          , old_shape.rend()
+          , new_shape.begin()
+        );
 
-    A.reshape(new_shape);
-    A.reshape(old_shape);
-  }
+        A.reshape(new_shape);
+        A.reshape(old_shape);
+    }
 };
 
-int run_generative_tests() {
-
-  run_storage_tests(null_modifier());
-  run_storage_tests(set_index_base_modifier());
-  run_storage_tests(reindex_modifier());
-  run_storage_tests(reshape_modifier());
-  std::cout << "Total Tests Run: " << tests_run << '\n';
-  return boost::exit_success;
+int run_generative_tests()
+{
+    unsigned int tests_run = run_storage_tests(null_modifier());
+    tests_run += run_storage_tests(set_index_base_modifier());
+    tests_run += run_storage_tests(reindex_modifier());
+    tests_run += run_storage_tests(reshape_modifier());
+    std::cout << "Total Tests Run: " << tests_run << '\n';
+    return boost::report_errors();
 }
 
 #endif // GENERATIVE_TESTS_RG072001_HPP
+
--- test/stl_interaction.cpp
+++ test/stl_interaction.cpp
@@ -1,7 +1,7 @@
 // Copyright 2002 The Trustees of Indiana University.
 
-// Use, modification and distribution is subject to the Boost Software 
-// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
 //  Boost.MultiArray Library
@@ -14,59 +14,59 @@
 // stl_interaction.cpp - Make sure multi_arrays work with STL containers.
 //
 
-#include "boost/test/minimal.hpp"
-
-#include "boost/multi_array.hpp"
+#include <boost/multi_array.hpp>
+#include <boost/core/lightweight_test.hpp>
 #include <algorithm>
 #include <vector>
 
-int
-test_main(int, char*[])
+int main(int, char*[])
 {
-  using boost::extents;
-  using boost::indices;
-  typedef boost::multi_array_types::index_range range;
-  typedef boost::multi_array<int,3> array3;
-  typedef boost::multi_array<int,2> array2;
-  
-  typedef std::vector<array3> array3vec;
+    typedef boost::multi_array_types::index_range range;
+    typedef boost::multi_array<int,3> array3;
+    typedef boost::multi_array<int,2> array2;
+    typedef std::vector<array3> array3vec;
 
-  int data[] = {
-    0,1,2,3,
-    4,5,6,7,
-    8,9,10,11,
+    int data[] =
+    {
+        0,1,2,3,
+        4,5,6,7,
+        8,9,10,11,
 
-    12,13,14,15,
-    16,17,18,19,
-    20,21,22,23
-  };
-  const int data_size = 24;
+        12,13,14,15,
+        16,17,18,19,
+        20,21,22,23
+    };
 
-  int insert[] = {
-    99,98,
-    97,96,
-  };
-  const int insert_size = 4;
-  array3 myarray(extents[2][3][4]);
-  myarray.assign(data,data+data_size);
+    int const data_size = 24;
 
-  array3vec myvec(5,myarray);
-  BOOST_CHECK(myarray == myvec[1]);
+    int insert[] =
+    {
+        99,98,
+        97,96,
+    };
 
-  array3::array_view<2>::type myview =
-    myarray[indices[1][range(0,2)][range(1,3)]];
+    int const insert_size = 4;
+    array3 myarray(boost::extents[2][3][4]);
+    myarray.assign(data, data + data_size);
 
-  array2 filler(extents[2][2]);
-  filler.assign(insert,insert+insert_size);
+    array3vec myvec(5, myarray);
+    BOOST_TEST(myarray == myvec[1]);
 
-  // Modify a portion of myarray through a view (myview)
-  myview = filler;
+    array3::array_view<2>::type myview = myarray[
+        boost::indices[1][range(0, 2)][range(1, 3)]
+    ];
 
+    array2 filler(boost::extents[2][2]);
+    filler.assign(insert, insert + insert_size);
 
-  myvec.push_back(myarray);
+    // Modify a portion of myarray through a view (myview)
+    myview = filler;
 
-  BOOST_CHECK(myarray != myvec[1]);
-  BOOST_CHECK(myarray == myvec[5]);
+    myvec.push_back(myarray);
 
-  return boost::exit_success;
+    BOOST_TEST(myarray != myvec[1]);
+    BOOST_TEST(myarray == myvec[5]);
+
+    return boost::report_errors();
 }
+
