043702ed6f653d46eca4913274f5aca32ec02b74
CromwellEnage <32967088+CromwellEnage@users.noreply.github.com>
2018-09-18 15:16:51 -0400

    Use <boost/core/lightweight_test.hpp>; Enable Automated Testing
    Switched from Boost.Test to Boost.Core lightweight_test component.
    Added AppVeyor & Travis Cl configuration files.

--- test/idxgen1.cpp
+++ test/idxgen1.cpp
@@ -1,7 +1,7 @@
 // Copyright 2002 The Trustees of Indiana University.
 
-// Use, modification and distribution is subject to the Boost Software 
-// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
 //  Boost.MultiArray Library
@@ -11,71 +11,67 @@
 //  See http://www.boost.org/libs/multi_array for documentation.
 
 //
-// idxset1.cpp - testing the code for index_gen
+// idxgen1.cpp - testing the code for index_gen
 //
 
-#include "boost/multi_array/index_gen.hpp"
-#include "boost/multi_array/index_range.hpp"
-#include "boost/multi_array/types.hpp"
-#include "boost/test/minimal.hpp"
-
-#include "boost/array.hpp"
+#include <boost/multi_array/index_gen.hpp>
+#include <boost/multi_array/index_range.hpp>
+#include <boost/multi_array/types.hpp>
+#include <boost/array.hpp>
+#include <boost/core/lightweight_test.hpp>
 
 typedef boost::detail::multi_array::index index_type;
 typedef boost::detail::multi_array::size_type size_type;
 typedef boost::detail::multi_array::index_range<index_type,size_type> range;
 
 template <int NumRanges, int NumDims>
-void check(const boost::detail::multi_array::
-           index_gen<NumRanges,NumDims>&) { }
-
-bool operator==(const range& lhs,const range& rhs) {  
-  return lhs.start_ == rhs.start_ &&
-    lhs.finish_ == rhs.finish_ &&
-    lhs.stride_ == rhs.stride_ &&
-    lhs.degenerate_ == rhs.degenerate_;
+void check(boost::detail::multi_array::index_gen<NumRanges,NumDims> const&)
+{
 }
 
-int
-test_main(int,char*[])
+bool operator==(range const& lhs, range const& rhs)
 {
+    return (lhs.start_ == rhs.start_) && (lhs.finish_ == rhs.finish_) &&
+    (lhs.stride_ == rhs.stride_) && (lhs.degenerate_ == rhs.degenerate_);
+}
 
-  boost::detail::multi_array::index_gen<0,0> indices;
+int main(int, char*[])
+{
+    boost::detail::multi_array::index_gen<0,0> indices;
 
+    check<1,1>(indices[range()]);
+    check<2,2>(indices[range()][range()]);
+    check<3,3>(indices[range()][range()][range()]);
 
-  check<1,1>(indices[range()]);
-  check<2,2>(indices[range()][range()]);
-  check<3,3>(indices[range()][range()][range()]);
+    check<1,0>(indices[0]);
+    check<2,0>(indices[0][0]);
+    check<2,1>(indices[range()][0]);
+    check<2,1>(indices[0][range()]);
+    check<3,0>(indices[0][0][0]);
+    check<3,1>(indices[range()][0][0]);
+    check<3,1>(indices[0][range()][0]);
+    check<3,1>(indices[0][0][range()]);
+    check<3,2>(indices[range()][range()][0]);
+    check<3,2>(indices[range()][0][range()]);
+    check<3,2>(indices[0][range()][range()]);
 
-  check<1,0>(indices[0]);
-  check<2,0>(indices[0][0]);
-  check<2,1>(indices[range()][0]);
-  check<2,1>(indices[0][range()]);
-  check<3,0>(indices[0][0][0]);
-  check<3,1>(indices[range()][0][0]);
-  check<3,1>(indices[0][range()][0]);
-  check<3,1>(indices[0][0][range()]);
-  check<3,2>(indices[range()][range()][0]);
-  check<3,2>(indices[range()][0][range()]);
-  check<3,2>(indices[0][range()][range()]);
+    {
+        boost::detail::multi_array::index_gen<3,3> is1 =
+            indices[range(0, 1, 2)][range(1, 2, 3)][range(2, 3, 4)];
+        BOOST_TEST((is1.ranges_[0] == (range(0, 1, 2))));
+        BOOST_TEST((is1.ranges_[1] == (range(1, 2, 3))));
+        BOOST_TEST((is1.ranges_[2] == (range(2, 3, 4))));
+    }
 
-  {
-    boost::detail::multi_array::index_gen<3,3> is1 =
-      indices[range(0,1,2)][range(1,2,3)][range(2,3,4)];
-    BOOST_CHECK(is1.ranges_[0] == range(0,1,2));
-    BOOST_CHECK(is1.ranges_[1] == range(1,2,3));
-    BOOST_CHECK(is1.ranges_[2] == range(2,3,4));
-  }
+    {
+        boost::detail::multi_array::index_gen<3,2> is2 = 
+            indices[range(0, 1, 2)][2][range(2, 3, 4)];
+        BOOST_TEST((is2.ranges_[0] == (range(0, 1, 2))));
+        BOOST_TEST(is2.ranges_[1] == range(2));
+        BOOST_TEST(is2.ranges_[1].is_degenerate());
+        BOOST_TEST((is2.ranges_[2] == (range(2, 3, 4))));
+    }
 
-  {
-    boost::detail::multi_array::index_gen<3,2> is2 = 
-      indices[range(0,1,2)][2][range(2,3,4)];
-    BOOST_CHECK(is2.ranges_[0] == range(0,1,2));
-    BOOST_CHECK(is2.ranges_[1] == range(2));
-    BOOST_CHECK(is2.ranges_[1].is_degenerate());
-    BOOST_CHECK(is2.ranges_[2] == range(2,3,4));
-  }
-
-  return boost::exit_success;
+    return boost::report_errors();
 }
 
--- test/reshape.cpp
+++ test/reshape.cpp
@@ -1,7 +1,7 @@
 // Copyright 2002 The Trustees of Indiana University.
 
-// Use, modification and distribution is subject to the Boost Software 
-// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
 //  Boost.MultiArray Library
@@ -14,84 +14,90 @@
 // reshape.cpp - testing reshaping functionality
 //
 
-#include "boost/multi_array.hpp"
+#include <boost/multi_array.hpp>
+#include <boost/array.hpp>
+#include <boost/type.hpp>
+#include <boost/core/lightweight_test.hpp>
 
-#include "boost/test/minimal.hpp"
-
-#include "boost/array.hpp"
-#include "boost/type.hpp"
-
-int
-test_main(int,char*[])
+int main(int, char*[])
 {
-  const int ndims=3;
-  typedef boost::multi_array<int,ndims> array;
-  typedef boost::multi_array_ref<int,ndims> array_ref;
-  typedef boost::const_multi_array_ref<int,ndims> const_array_ref;
+    int const ndims = 3;
+    typedef boost::multi_array<int,ndims> array;
+    typedef boost::multi_array_ref<int,ndims> array_ref;
+    typedef boost::const_multi_array_ref<int,ndims> const_array_ref;
 
-  boost::array<array::size_type,ndims> dims = {{2,3,4}};
-  boost::array<array::size_type,ndims> new_dims = {{4,3,2}};
+    boost::array<array::size_type,ndims> dims = {{2, 3, 4}};
+    boost::array<array::size_type,ndims> new_dims = {{4, 3, 2}};
 
-  int data[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,
-                 14,15,16,17,18,19,20,21,22,23};
-  const int data_size=24;
+    int data[] =
+    {
+        0,1,2,3,4,5,6,7,8,9,10,11,12,13,
+        14,15,16,17,18,19,20,21,22,23
+    };
+    int const data_size = 24;
 
-  // Basic reshape test
-  {
-    array A(dims);
-    A.assign(data,data+data_size);
+    // Basic reshape test
+    {
+        array A(dims);
+        A.assign(data, data + data_size);
 
-    array_ref B(data,dims);
-    const_array_ref C(data,dims);
+        array_ref B(data, dims);
+        const_array_ref C(data, dims);
 
-    A.reshape(new_dims);
-    B.reshape(new_dims);
-    C.reshape(new_dims);
+        A.reshape(new_dims);
+        B.reshape(new_dims);
+        C.reshape(new_dims);
 
-    int* ptr = data;
-    for (array::index i = 0; i != 4; ++i)
-      for (array::index j = 0; j != 3; ++j)
-        for (array::index k = 0; k != 2; ++k) {
-          BOOST_CHECK(A[i][j][k] == *ptr);
-          BOOST_CHECK(B[i][j][k] == *ptr);
-          BOOST_CHECK(C[i][j][k] == *ptr++);
+        int* ptr = data;
+
+        for (array::index i = 0; i != 4; ++i)
+        {
+            for (array::index j = 0; j != 3; ++j)
+            {
+                for (array::index k = 0; k != 2; ++k)
+                {
+                    BOOST_TEST_EQ(A[i][j][k], *ptr);
+                    BOOST_TEST_EQ(B[i][j][k], *ptr);
+                    BOOST_TEST_EQ(C[i][j][k], *ptr++);
+                }
+            }
         }
-  }
+    }
 
-  // Ensure that index bases are preserved over reshape
-  {
-    boost::array<array::index,ndims> bases = {{0, 1, -1}};
+    // Ensure that index bases are preserved over reshape
+    {
+        boost::array<array::index,ndims> bases = {{0, 1, -1}};
 
-    array A(dims);
-    A.assign(data,data+data_size);
+        array A(dims);
+        A.assign(data, data + data_size);
 
-    array_ref B(data,dims);
-    const_array_ref C(data,dims);
+        array_ref B(data, dims);
+        const_array_ref C(data, dims);
 
-    A.reindex(bases);
-    B.reindex(bases);
-    C.reindex(bases);
+        A.reindex(bases);
+        B.reindex(bases);
+        C.reindex(bases);
 
-    A.reshape(new_dims);
-    B.reshape(new_dims);
-    C.reshape(new_dims);
+        A.reshape(new_dims);
+        B.reshape(new_dims);
+        C.reshape(new_dims);
 
-    int* ptr = data;
-    for (array::index i = 0; i != 4; ++i)
-      for (array::index j = 1; j != 4; ++j)
-        for (array::index k = -1; k != 1; ++k) {
-          BOOST_CHECK(A[i][j][k] == *ptr);
-          BOOST_CHECK(B[i][j][k] == *ptr);
-          BOOST_CHECK(C[i][j][k] == *ptr++);
+        int* ptr = data;
+
+        for (array::index i = 0; i != 4; ++i)
+        {
+            for (array::index j = 1; j != 4; ++j)
+            {
+                for (array::index k = -1; k != 1; ++k)
+                {
+                    BOOST_TEST_EQ(A[i][j][k], *ptr);
+                    BOOST_TEST_EQ(B[i][j][k], *ptr);
+                    BOOST_TEST_EQ(C[i][j][k], *ptr++);
+                }
+            }
         }
-  }
-  
-  return boost::exit_success;
+    }
+
+    return boost::report_errors();
 }
-
-
-
-
-
-
 
