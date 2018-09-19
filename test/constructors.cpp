b3cb12e223cbbeb5395ef6ebdcf4351c840aa766
CromwellEnage <32967088+CromwellEnage@users.noreply.github.com>
2018-09-18 22:49:41 -0400

    Revert "Documentation Cleanup"
    This reverts commit 3c85a0b5428e21e8ae672b139ab073479899df07.

--- test/constructors.cpp
+++ test/constructors.cpp
@@ -1,7 +1,7 @@
 // Copyright 2002 The Trustees of Indiana University.
 
-// Use, modification and distribution is subject to the Boost Software
-// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
 //  Boost.MultiArray Library
@@ -14,205 +14,223 @@
 // constructors.cpp - Testing out the various constructor options
 //
 
-
-#include "boost/test/minimal.hpp"
-
-#include "boost/multi_array.hpp"
+#include <boost/multi_array.hpp>
+#include <boost/core/lightweight_test.hpp>
 #include <algorithm>
 #include <list>
 
-void check_shape(const double&, std::size_t*, int*, unsigned int)
-{}
-
-template <class Array>
-void check_shape(const Array& A,
-                 std::size_t* sizes,
-                 int* strides,
-                 unsigned int num_elements)
+void check_shape(double const&, std::size_t*, int*, unsigned long)
 {
-  BOOST_CHECK(A.num_elements() == num_elements);
-  BOOST_CHECK(A.size() == *sizes);
-  BOOST_CHECK(std::equal(sizes, sizes + A.num_dimensions(), A.shape()));
-  BOOST_CHECK(std::equal(strides, strides + A.num_dimensions(), A.strides()));
-  check_shape(A[0], ++sizes, ++strides, num_elements / A.size());
 }
 
-
-bool equal(const double& a, const double& b)
+template <typename Array>
+void
+    check_shape(
+        Array const& A
+      , std::size_t* sizes
+      , int* strides
+      , unsigned long num_elements
+    )
 {
-  return a == b;
+    BOOST_TEST_EQ(A.num_elements(), num_elements);
+    BOOST_TEST_EQ(A.size(), *sizes);
+    BOOST_TEST((
+        std::equal(sizes, sizes + A.num_dimensions(), A.shape())
+    ));
+    BOOST_TEST((
+        std::equal(strides, strides + A.num_dimensions(), A.strides())
+    ));
+    check_shape(A[0], ++sizes, ++strides, num_elements / A.size());
+}
+
+bool equal(double const& a, double const& b)
+{
+    return a == b;
 }
 
 template <typename ArrayA, typename ArrayB>
-bool equal(const ArrayA& A, const ArrayB& B)
+bool equal(ArrayA const& A, ArrayB const& B)
 {
-  typename ArrayA::const_iterator ia;
-  typename ArrayB::const_iterator ib = B.begin();
-  for (ia = A.begin(); ia != A.end(); ++ia, ++ib)
-    if (!::equal(*ia, *ib))
-      return false;
-  return true;
+    typename ArrayA::const_iterator ia;
+    typename ArrayB::const_iterator ib = B.begin();
+
+    for (ia = A.begin(); ia != A.end(); ++ia, ++ib)
+    {
+        if (!::equal(*ia, *ib))
+        {
+            return false;
+        }
+    }
+
+    return true;
 }
 
-
-int
-test_main(int, char*[])
+int main(int, char*[])
 {
-  typedef boost::multi_array<double, 3>::size_type size_type;
-  boost::array<size_type,3> sizes = { { 3, 3, 3 } };
-  int strides[] = { 9, 3, 1 };
-  size_type num_elements = 27;
-
-  // Default multi_array constructor
-  {
-    boost::multi_array<double, 3> A;
-  }
-
-  // Constructor 1, default storage order and allocator
-  {
-    boost::multi_array<double, 3> A(sizes);
-    check_shape(A, &sizes[0], strides, num_elements);
-
-    double* ptr = 0;
-    boost::multi_array_ref<double,3> B(ptr,sizes);
-    check_shape(B, &sizes[0], strides, num_elements);
-
-    const double* cptr = ptr;
-    boost::const_multi_array_ref<double,3> C(cptr,sizes);
-    check_shape(C, &sizes[0], strides, num_elements);
-  }
-
-  // Constructor 1, fortran storage order and user-supplied allocator
-  {
-    typedef boost::multi_array<double, 3,
-      std::allocator<double> >::size_type size_type;
-    size_type num_elements = 27;
-    int col_strides[] = { 1, 3, 9 };
-
-    boost::multi_array<double, 3,
-      std::allocator<double> > A(sizes,boost::fortran_storage_order());
-    check_shape(A, &sizes[0], col_strides, num_elements);
-
-    double *ptr=0;
-    boost::multi_array_ref<double, 3>
-      B(ptr,sizes,boost::fortran_storage_order());
-    check_shape(B, &sizes[0], col_strides, num_elements);
-
-    const double *cptr=ptr;
-    boost::const_multi_array_ref<double, 3>
-      C(cptr,sizes,boost::fortran_storage_order());
-    check_shape(C, &sizes[0], col_strides, num_elements);
-  }
-
-  // Constructor 2, default storage order and allocator
-  {
-    typedef boost::multi_array<double, 3>::size_type size_type;
+    typedef boost::multi_array<double,3>::size_type size_type;
+    boost::array<size_type,3> sizes = {{3, 3, 3}};
+    int strides[] = {9, 3, 1};
     size_type num_elements = 27;
 
-    boost::multi_array<double, 3>::extent_gen extents;
-    boost::multi_array<double, 3> A(extents[3][3][3]);
-    check_shape(A, &sizes[0], strides, num_elements);
+    // Default multi_array constructor
+    {
+        boost::multi_array<double,3> A;
+    }
 
-    double *ptr=0;
-    boost::multi_array_ref<double, 3> B(ptr,extents[3][3][3]);
-    check_shape(B, &sizes[0], strides, num_elements);
+    // Constructor 1, default storage order and allocator
+    {
+        boost::multi_array<double,3> A(sizes);
+        check_shape(A, &sizes[0], strides, num_elements);
 
-    const double *cptr=ptr;
-    boost::const_multi_array_ref<double, 3> C(cptr,extents[3][3][3]);
-    check_shape(C, &sizes[0], strides, num_elements);
-  }
+        double* ptr = 0;
+        boost::multi_array_ref<double,3> B(ptr, sizes);
+        check_shape(B, &sizes[0], strides, num_elements);
 
-  // Copy Constructors
-  {
-    typedef boost::multi_array<double, 3>::size_type size_type;
-    size_type num_elements = 27;
-    std::vector<double> vals(27, 4.5);
+        double const* cptr = ptr;
+        boost::const_multi_array_ref<double,3> C(cptr, sizes);
+        check_shape(C, &sizes[0], strides, num_elements);
+    }
 
-    boost::multi_array<double, 3> A(sizes);
-    A.assign(vals.begin(),vals.end());
-    boost::multi_array<double, 3> B(A);
-    check_shape(B, &sizes[0], strides, num_elements);
-    BOOST_CHECK(::equal(A, B));
+    // Constructor 1, fortran storage order and user-supplied allocator
+    {
+        typedef boost::multi_array<
+            double
+          , 3
+          , std::allocator<double>
+        >::size_type size_type;
+        size_type num_elements = 27;
+        int col_strides[] = {1, 3, 9};
 
-    double ptr[27];
-    boost::multi_array_ref<double, 3> C(ptr,sizes);
-    A.assign(vals.begin(),vals.end());
-    boost::multi_array_ref<double, 3> D(C);
-    check_shape(D, &sizes[0], strides, num_elements);
-    BOOST_CHECK(C.data() == D.data());
+        boost::multi_array<
+            double
+          , 3
+          , std::allocator<double>
+        > A(sizes, boost::fortran_storage_order());
+        check_shape(A, &sizes[0], col_strides, num_elements);
 
-    const double* cptr = ptr;
-    boost::const_multi_array_ref<double, 3> E(cptr,sizes);
-    boost::const_multi_array_ref<double, 3> F(E);
-    check_shape(F, &sizes[0], strides, num_elements);
-    BOOST_CHECK(E.data() == F.data());
-  }
+        double* ptr = 0;
+        boost::multi_array_ref<double,3> B(
+            ptr
+          , sizes
+          , boost::fortran_storage_order()
+        );
+        check_shape(B, &sizes[0], col_strides, num_elements);
 
+        double const* cptr = ptr;
+        boost::const_multi_array_ref<double,3> C(
+            cptr
+          , sizes
+          , boost::fortran_storage_order()
+        );
+        check_shape(C, &sizes[0], col_strides, num_elements);
+    }
 
-  // Conversion construction
-  {
-    typedef boost::multi_array<double, 3>::size_type size_type;
-    size_type num_elements = 27;
-    std::vector<double> vals(27, 4.5);
+    // Constructor 2, default storage order and allocator
+    {
+        typedef boost::multi_array<double,3>::size_type size_type;
+        size_type num_elements = 27;
 
-    boost::multi_array<double, 3> A(sizes);
-    A.assign(vals.begin(),vals.end());
-    boost::multi_array_ref<double, 3> B(A);
-    boost::const_multi_array_ref<double, 3> C(A);
-    check_shape(B, &sizes[0], strides, num_elements);
-    check_shape(C, &sizes[0], strides, num_elements);
-    BOOST_CHECK(B.data() == A.data());
-    BOOST_CHECK(C.data() == A.data());
+        boost::multi_array<double,3>::extent_gen extents;
+        boost::multi_array<double,3> A(extents[3][3][3]);
+        check_shape(A, &sizes[0], strides, num_elements);
 
-    double ptr[27];
-    boost::multi_array_ref<double, 3> D(ptr,sizes);
-    D.assign(vals.begin(),vals.end());
-    boost::const_multi_array_ref<double, 3> E(D);
-    check_shape(E, &sizes[0], strides, num_elements);
-    BOOST_CHECK(E.data() == D.data());
-  }
+        double* ptr = 0;
+        boost::multi_array_ref<double,3> B(ptr, extents[3][3][3]);
+        check_shape(B, &sizes[0], strides, num_elements);
 
-  // Assignment Operator
-  {
-    typedef boost::multi_array<double, 3>::size_type size_type;
-    size_type num_elements = 27;
-    std::vector<double> vals(27, 4.5);
+        double const* cptr = ptr;
+        boost::const_multi_array_ref<double,3> C(cptr, extents[3][3][3]);
+        check_shape(C, &sizes[0], strides, num_elements);
+    }
 
-    boost::multi_array<double, 3> A(sizes), B(sizes);
-    A.assign(vals.begin(),vals.end());
-    B = A;
-    check_shape(B, &sizes[0], strides, num_elements);
-    BOOST_CHECK(::equal(A, B));
+    // Copy Constructors
+    {
+        typedef boost::multi_array<double,3>::size_type size_type;
+        size_type num_elements = 27;
+        std::vector<double> vals(27, 4.5);
 
-    double ptr1[27];
-    double ptr2[27];
-    boost::multi_array_ref<double, 3> C(ptr1,sizes), D(ptr2,sizes);
-    C.assign(vals.begin(),vals.end());
-    D = C;
-    check_shape(D, &sizes[0], strides, num_elements);
-    BOOST_CHECK(::equal(C,D));
-  }
+        boost::multi_array<double,3> A(sizes);
+        A.assign(vals.begin(),vals.end());
+        boost::multi_array<double,3> B(A);
+        check_shape(B, &sizes[0], strides, num_elements);
+        BOOST_TEST((::equal(A, B)));
 
+        double ptr[27];
+        boost::multi_array_ref<double,3> C(ptr, sizes);
+        A.assign(vals.begin(), vals.end());
+        boost::multi_array_ref<double,3> D(C);
+        check_shape(D, &sizes[0], strides, num_elements);
+        BOOST_TEST_EQ(C.data(), D.data());
 
-  // subarray value_type is multi_array
-  {
-    typedef boost::multi_array<double,3> array;
-    typedef array::size_type size_type;
-    size_type num_elements = 27;
-    std::vector<double> vals(num_elements, 4.5);
+        double const* cptr = ptr;
+        boost::const_multi_array_ref<double,3> E(cptr, sizes);
+        boost::const_multi_array_ref<double,3> F(E);
+        check_shape(F, &sizes[0], strides, num_elements);
+        BOOST_TEST_EQ(E.data(), F.data());
+    }
 
-    boost::multi_array<double, 3> A(sizes);
-    A.assign(vals.begin(),vals.end());
+    // Conversion construction
+    {
+        typedef boost::multi_array<double,3>::size_type size_type;
+        size_type num_elements = 27;
+        std::vector<double> vals(27, 4.5);
 
-    typedef array::subarray<2>::type subarray;
-    subarray B = A[1];
-    subarray::value_type C = B[0];
+        boost::multi_array<double,3> A(sizes);
+        A.assign(vals.begin(), vals.end());
+        boost::multi_array_ref<double,3> B(A);
+        boost::const_multi_array_ref<double,3> C(A);
+        check_shape(B, &sizes[0], strides, num_elements);
+        check_shape(C, &sizes[0], strides, num_elements);
+        BOOST_TEST_EQ(B.data(), A.data());
+        BOOST_TEST_EQ(C.data(), A.data());
 
-    // should comparisons between the types work?
-    BOOST_CHECK(::equal(A[1][0],C));
-    BOOST_CHECK(::equal(B[0],C));
-  }
-  return boost::exit_success;
+        double ptr[27];
+        boost::multi_array_ref<double,3> D(ptr, sizes);
+        D.assign(vals.begin(), vals.end());
+        boost::const_multi_array_ref<double,3> E(D);
+        check_shape(E, &sizes[0], strides, num_elements);
+        BOOST_TEST_EQ(E.data(), D.data());
+    }
+
+    // Assignment Operator
+    {
+        typedef boost::multi_array<double,3>::size_type size_type;
+        size_type num_elements = 27;
+        std::vector<double> vals(27, 4.5);
+
+        boost::multi_array<double,3> A(sizes), B(sizes);
+        A.assign(vals.begin(), vals.end());
+        B = A;
+        check_shape(B, &sizes[0], strides, num_elements);
+        BOOST_TEST((::equal(A, B)));
+        double ptr1[27];
+        double ptr2[27];
+        boost::multi_array_ref<double,3> C(ptr1, sizes), D(ptr2, sizes);
+        C.assign(vals.begin(), vals.end());
+        D = C;
+        check_shape(D, &sizes[0], strides, num_elements);
+        BOOST_TEST((::equal(C, D)));
+    }
+
+    // subarray value_type is multi_array
+    {
+        typedef boost::multi_array<double,3> array;
+        typedef array::size_type size_type;
+        size_type num_elements = 27;
+        std::vector<double> vals(num_elements, 4.5);
+
+        boost::multi_array<double,3> A(sizes);
+        A.assign(vals.begin(), vals.end());
+
+        typedef array::subarray<2>::type subarray;
+        subarray B = A[1];
+        subarray::value_type C = B[0];
+
+        // should comparisons between the types work?
+        BOOST_TEST((::equal(A[1][0], C)));
+        BOOST_TEST((::equal(B[0], C)));
+    }
+
+    return boost::report_errors();
 }
-
 
--- test/range1.cpp
+++ test/range1.cpp
@@ -1,7 +1,7 @@
 // Copyright 2002 The Trustees of Indiana University.
 
-// Use, modification and distribution is subject to the Boost Software 
-// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
 //  Boost.MultiArray Library
@@ -14,92 +14,98 @@
 // range1.cpp - test of index_range
 //
 
-
-#include "boost/multi_array/index_range.hpp"
-
-#include "boost/test/minimal.hpp"
-
-#include "boost/array.hpp"
+#include <boost/multi_array/index_range.hpp>
+#include <boost/array.hpp>
+#include <boost/core/lightweight_test.hpp>
 #include <cstddef>
 
-int
-test_main(int,char*[])
+int main(int, char*[])
 {
-  typedef boost::detail::multi_array::index_range<int,std::size_t> range;
+    typedef boost::detail::multi_array::index_range<int,std::size_t> range;
 
-  {
-    // typical range creation and extraction
-    range r1(-3,5);
-    BOOST_CHECK(r1.start() == -3);
-    BOOST_CHECK(r1.finish() == 5);
-    BOOST_CHECK(r1.stride() == 1);
-    BOOST_CHECK(!r1.is_degenerate());
-    BOOST_CHECK(r1.get_start(0) == -3);
-    BOOST_CHECK(r1.get_finish(100) == 5);
-  }
+    {
+        // typical range creation and extraction
+        range r1(-3, 5);
+        BOOST_TEST_EQ(r1.start(), -3);
+        BOOST_TEST_EQ(r1.finish(), 5);
+        BOOST_TEST_EQ(r1.stride(), 1);
+        BOOST_TEST_EQ(r1.size(0), 8);
+        BOOST_TEST(!r1.is_degenerate());
+        BOOST_TEST_EQ(r1.get_start(0), -3);
+        BOOST_TEST_EQ(r1.get_finish(100), 5);
+    }
 
-  {
-    range r2(-3,5,2);
-    BOOST_CHECK(r2.start() == -3);
-    BOOST_CHECK(r2.finish() == 5);
-    BOOST_CHECK(r2.stride() == 2);
-    BOOST_CHECK(!r2.is_degenerate());
-  }
+    {
+        range r2(-3, 5, 2);
+        BOOST_TEST_EQ(r2.start(), -3);
+        BOOST_TEST_EQ(r2.finish(), 5);
+        BOOST_TEST_EQ(r2.stride(), 2);
+        BOOST_TEST_EQ(r2.size(0), 4);
+        BOOST_TEST(!r2.is_degenerate());
+    }
 
-  {
-    // degenerate creation
-    range r3(5);
-    BOOST_CHECK(r3.start() == 5);
-    BOOST_CHECK(r3.finish() == 6);
-    BOOST_CHECK(r3.stride() == 1);
-    BOOST_CHECK(r3.is_degenerate());
-  }
+    {
+        // degenerate creation
+        range r3(5);
+        BOOST_TEST_EQ(r3.start(), 5);
+        BOOST_TEST_EQ(r3.finish(), 6);
+        BOOST_TEST_EQ(r3.stride(), 1);
+        BOOST_TEST_EQ(r3.size(0), 1);
+        BOOST_TEST(r3.is_degenerate());
+    }
 
-  {
-    // default range creation
-    range r4;
-    BOOST_CHECK(r4.get_start(0) == 0);
-    BOOST_CHECK(r4.get_finish(100) == 100);
-    BOOST_CHECK(r4.stride() == 1);
-  }
+    {
+        // default range creation
+        range r4;
+        BOOST_TEST_EQ(r4.get_start(0), 0);
+        BOOST_TEST_EQ(r4.get_finish(100), 100);
+        BOOST_TEST_EQ(r4.stride(), 1);
+        BOOST_TEST_EQ(r4.size(0), 0);
+    }
 
-  {
-    // create a range using the setter methods
-    range r5 = range().stride(2).start(-3).finish(7);
-    BOOST_CHECK(r5.start() == -3);
-    BOOST_CHECK(r5.stride() == 2);
-    BOOST_CHECK(r5.finish() == 7);
-  }
+    {
+        // create a range using the setter methods
+        range r5 = range().stride(2).start(-3).finish(7);
+        BOOST_TEST_EQ(r5.start(), -3);
+        BOOST_TEST_EQ(r5.stride(), 2);
+        BOOST_TEST_EQ(r5.finish(), 7);
+        BOOST_TEST_EQ(r5.size(0), 5);
+    }
 
-  // try out all the comparison operators
-  {
-    range r6 = -3 <= range().stride(2) < 7;
-    BOOST_CHECK(r6.start() == -3);
-    BOOST_CHECK(r6.stride() == 2);
-    BOOST_CHECK(r6.finish() == 7);
-  }
+    // try out all the comparison operators
+    {
+        range r6 = -3 <= range().stride(2) < 7;
+        BOOST_TEST_EQ(r6.start(), -3);
+        BOOST_TEST_EQ(r6.stride(), 2);
+        BOOST_TEST_EQ(r6.finish(), 7);
+        BOOST_TEST_EQ(r6.size(0), 5);
+    }
 
-  {
-    range r7 = -3 < range() <= 7;
-    BOOST_CHECK(r7.start() == -2);
-    BOOST_CHECK(r7.stride() == 1);
-    BOOST_CHECK(r7.finish() == 8);
-  }
+    {
+        range r7 = -3 < range() <= 7;
+        BOOST_TEST_EQ(r7.start(), -2);
+        BOOST_TEST_EQ(r7.stride(), 1);
+        BOOST_TEST_EQ(r7.finish(), 8);
+        BOOST_TEST_EQ(r7.size(0), 10);
+    }
 
-  // arithmetic operators
-  {
-    range r8 = range(0,5) + 2;
-    BOOST_CHECK(r8.start() == 2);
-    BOOST_CHECK(r8.stride() == 1);
-    BOOST_CHECK(r8.finish() == 7);
-  }
+    // arithmetic operators
+    {
+        range r8 = range(0, 5) + 2;
+        BOOST_TEST_EQ(r8.start(), 2);
+        BOOST_TEST_EQ(r8.stride(), 1);
+        BOOST_TEST_EQ(r8.finish(), 7);
+        BOOST_TEST_EQ(r8.size(0), 5);
+    }
 
-  {
-    range r9 = range(0,5) - 2;
-    BOOST_CHECK(r9.start() == -2);
-    BOOST_CHECK(r9.stride() == 1);
-    BOOST_CHECK(r9.finish() == 3);
-  }
+    {
+        range r9 = range(0, 5) - 2;
+        BOOST_TEST_EQ(r9.start(), -2);
+        BOOST_TEST_EQ(r9.stride(), 1);
+        BOOST_TEST_EQ(r9.finish(), 3);
+        BOOST_TEST_EQ(r9.size(0), 5);
+    }
 
-  return boost::exit_success;
+    return boost::report_errors();
 }
+
