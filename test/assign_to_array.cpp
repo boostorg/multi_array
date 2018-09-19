3c85a0b5428e21e8ae672b139ab073479899df07
CromwellEnage <32967088+CromwellEnage@users.noreply.github.com>
2018-09-18 22:38:56 -0400

    Documentation Cleanup
    HTML tidying.
    A few documentation links currently point to pages that HP retired along with the rest of the SGI STL website.  These links now point to Robert Ramey's mirror site.

--- test/assign_to_array.cpp
+++ test/assign_to_array.cpp
@@ -1,7 +1,7 @@
 // Copyright 2002 The Trustees of Indiana University.
 
-// Distributed under the Boost Software License, Version 1.0.
-// (See accompanying file LICENSE_1_0.txt or copy at
+// Use, modification and distribution is subject to the Boost Software
+// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
 //  Boost.MultiArray Library
@@ -14,53 +14,47 @@
 // assign_to_array.cpp - multi_array should be constructible from any other
 // array type in the library.
 //
-
+//
 #include "generative_tests.hpp"
-#include <boost/array.hpp>
-#include <boost/cstdlib.hpp>
+#include "boost/array.hpp"
+#include "boost/multi_array.hpp"
+#include "boost/cstdlib.hpp"
 #include <algorithm>
 #include <iostream>
 
-bool equal(int const& a, int const& b)
+bool equal(const int& a, const int& b)
 {
-    return a == b;
+  return a == b;
 }
 
 template <typename ArrayA, typename ArrayB>
-bool equal(ArrayA const& A, ArrayB const& B)
+bool equal(const ArrayA& A, const ArrayB& B)
 {
-    typename ArrayA::const_iterator ia;
-    typename ArrayB::const_iterator ib = B.begin();
+  typename ArrayA::const_iterator ia;
+  typename ArrayB::const_iterator ib = B.begin();
+  for (ia = A.begin(); ia != A.end(); ++ia, ++ib)
+    if (!::equal(*ia, *ib))
+      return false;
+  return true;
+}
 
-    for (ia = A.begin(); ia != A.end(); ++ia, ++ib)
-    {
-        if (!::equal(*ia, *ib))
-        {
-            return false;
-        }
-    }
 
-    return true;
+template <typename Array>
+void access(Array& A, const mutable_array_tag&) {
+
+  assign(A);
+  access(A,const_array_tag());
 }
 
 template <typename Array>
-unsigned int access(Array& A, mutable_array_tag const&)
-{
-    assign(A);
-    return access(A, const_array_tag());
+void access(Array& A, const const_array_tag&) {
+  typedef boost::multi_array<int,3> array3;
+  array3 acopy(A);
+  BOOST_CHECK(::equal(acopy,A));
+  ++tests_run;
 }
 
-template <typename Array>
-unsigned int access(Array& A, const_array_tag const&)
-{
-    typedef boost::multi_array<int,3> array3;
-    array3 acopy(A);
-    BOOST_TEST((::equal(acopy,A)));
-    return 1;
-}
 
-int main(int, char*[])
-{
-    return run_generative_tests();
+int test_main(int,char*[]) {
+  return run_generative_tests();
 }
-
